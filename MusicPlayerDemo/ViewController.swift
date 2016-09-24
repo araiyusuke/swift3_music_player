276c04dbbac05bea575e0265646c08bd6aa55fa2
yusuke arai <info.yusuke.arai@gmail.com>
2016-09-25 01:17:16 +0900

    Initial Commit

---
+++ MusicPlayerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicPlayerDemo
+//
+//  Created by yusukearai on 2016/09/25.
+//  Copyright © 2016年 yusuke arai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
